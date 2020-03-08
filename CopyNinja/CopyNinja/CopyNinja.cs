f4b17046a9e625b5ec3d3d40ade358e734f81395
Khanimamba <khanimamba@hotmail.com>
2020-03-08 18:58:33 +0200

    Initial commit : Created Test Project. Added upload functionality

---
+++ CopyNinja/CopyNinja/CopyNinja.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Text;
+
+namespace CopyNinja
+{
+    public class CopyNinja
+    {
+
+        public static void Copy(string url, string username,string password,string filename, string localPath)
+        {
+            var client = new RestClient(url)
+            {
+                UserAgent = "Sia-Agent",
+                Authenticator = new HttpBasicAuthenticator(username, password)
+            };
+
+            var request = new RestRequest()
+            {
+                Method = Method.POST,
+                RequestFormat = DataFormat.Json
+            };
+
+            request.Resource = "/clipboard/{filename}";
+            request.AddParameter("filename", filename, ParameterType.UrlSegment);
+            request.AddFile("file", localPath);
+
+            var response = client.Execute(request);
+        } 
+
+
+
+
+
+
+
+    }
+}
